a7d435cb1dbedf8798b2d9aee58168fa977c5adc
Eren <114621741+DalgaGamePlay@users.noreply.github.com>
2023-11-22 21:29:28 +0300

    Update about.tsx

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -4,7 +4,15 @@ export default function About() {
   return (
     <Card className="z-10 gap-11 space-y-4 w-full text-sm text-white bg-opacity-50 select-none hover:scale-105 max-w-[35rem]">
       <CardBody className="w-full text-center break-words text-wrap">
-       Hello, I'm Eren. I am a high school student and I am a freshman in high school. My goal is to become a computer engineer. One of the things I like is building sites using React and Express frameworks. I also love playing computer games, especially games such as Craft Rise and Forest interest me. You can visit my social media accounts for more information.
+        As a Software Developer with a associate degree in computer programming
+        from Gazi University, I am passionate about developing innovative and
+        user-friendly applications. I am currently working on enhancing my
+        skills in full-stack web development to expand my knowledge and
+        expertise in this field. I am interested in learning new technologies
+        and frameworks that can help me create more efficient and effective web
+        applications. I am also eager to apply my skills and knowledge to
+        real-world problems and challenges, and to collaborate with other
+        professionals and experts in the field.
       </CardBody>
     </Card>
   );
